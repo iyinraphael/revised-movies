8b932061332c4c0aa6239e0864b27b4189e2886d
iyinraphael <iyinraphael@gmail.com>
2020-06-04 08:14:24 -0700

    initial set up

---
+++ MyMovies/Model Controllers/MovieController.swift
@@ -0,0 +1,64 @@
+//
+//  MovieController.swift
+//  MyMovies
+//
+//  Created by Spencer Curtis on 8/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case otherError
+    case noData
+    case failedDecode
+}
+
+class MovieController {
+    
+    private let apiKey = "4cc920dab8b729a619647ccc4d191d5e"
+    private let baseURL = URL(string: "https://api.themoviedb.org/3/search/movie")!
+    
+    typealias CompletionHandler = (Result<Bool, NetworkError>) -> Void
+    
+    // MARK: - Properties
+    
+    var searchedMovies: [MovieDBMovie] = []
+    
+    // MARK: - TheMovieDB API
+    
+    func searchForMovie(with searchTerm: String, completion: @escaping CompletionHandler) {
+        var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let queryParameters = ["query": searchTerm,
+                               "api_key": apiKey]
+        components?.queryItems = queryParameters.map({URLQueryItem(name: $0.key, value: $0.value)})
+        
+        guard let requestURL = components?.url else {
+            completion(.failure(.otherError))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Error searching for movie with search term \(searchTerm): \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let movieDBMovies = try JSONDecoder().decode(MovieDBResults.self, from: data).results
+                self.searchedMovies = movieDBMovies
+                completion(.success(true))
+            } catch {
+                NSLog("Error decoding JSON data: \(error)")
+                completion(.failure(.failedDecode))
+            }
+        }.resume()
+    }
+}
